a8f3a14867ae63d953b6502548ec6a1fd112ef3a
nikhil.leo <l00170961@atu.ie>
2023-11-22 22:44:46 +0000

    JAR added

--- obj/Debug/net8.0/WeatherAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10992cd85d2afab1a15d67475cc449c3b368353b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
